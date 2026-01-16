6fd4d822a9a41b4744f5518ebe57604ac3e42c09
Hayder Hassan <hayderh@gmail.com>
2026-01-16 04:22:53 +0000

    fix(queries): fix queries (#3)
    Co-authored-by: Hayder Hassan <hayder.hassan@bt.com>

--- queries/folds.scm
+++ queries/folds.scm
@@ -1,28 +1,7 @@
-; Foldable blocks
 (sub_definition) @fold
 
 (function_definition) @fold
 
-(property_get_definition) @fold
-
-(property_let_definition) @fold
-
-(property_set_definition) @fold
-
-(type_definition) @fold
-
-(enum_definition) @fold
-
 (if_statement) @fold
 
-(select_case_statement) @fold
-
 (for_statement) @fold
-
-(for_each_statement) @fold
-
-(while_statement) @fold
-
-(do_statement) @fold
-
-(with_statement) @fold
--- queries/indents.scm
+++ queries/indents.scm
@@ -1,32 +1,11 @@
-; Increase indent after these constructs
 [
   (sub_definition)
   (function_definition)
-  (property_get_definition)
-  (property_let_definition)
-  (property_set_definition)
-  (type_definition)
-  (enum_definition)
   (if_statement)
-  (elseif_clause)
-  (else_clause)
-  (select_case_statement)
-  (case_clause)
-  (case_else_clause)
   (for_statement)
-  (for_each_statement)
-  (while_statement)
-  (do_statement)
-  (with_statement)
 ] @indent.begin
 
-; Decrease indent at these endings (now named keyword tokens)
-[
-  (kw_end)
-  (kw_loop)
-  (kw_wend)
-  (kw_next)
-] @indent.end
-
-; Labels outdent to column 0
 (label_statement) @indent.branch
+
+; End-dedent can be inferred by Zed from @indent.begin in many cases.
+; Keeping it simple avoids node-type mismatches.
